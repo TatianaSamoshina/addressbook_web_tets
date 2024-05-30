9f6560321c261b3bcc4ae8a201fb4e430b92e3f0
TatianaSamoshina <tatjana.kolesnickowa@yandex.ru>
2024-05-30 22:02:04 +0300

    17.1

--- addressbook_web_tets/tests/AddContactToGroupTests.cs
+++ addressbook_web_tets/tests/AddContactToGroupTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿//using System;
 using System.Collections.Generic;
 using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace addressbook_web_tets 
@@ -11,11 +9,35 @@ namespace addressbook_web_tets
     {
         [Test]
         public void AddContactToGroupTest() 
-        {
+        {           
+            // Проверка наличия групп, если нет - создаем
+            if (GroupDatacs.GetAll().Count == 0)
+            {
+                app.Groups.Create(new GroupDatacs("TestGroup1", "TestGroup2", "TestGroup3"));
+            }
+
             GroupDatacs group = GroupDatacs.GetAll()[0];
             List<ContactDatas> oldList = group.GetContacts();
-            ContactDatas contact = ContactDatas.GetAll().Except(oldList).First();
+
+            // Проверка наличия контактов, если нет - создаем
+            if (ContactDatas.GetAll().Count == 0)
+            {
+                app.Contacts.Create(new ContactDatas("Test", "Test2"));
+            }
+
+            // Поиск контакта, который еще не в группе
+            ContactDatas contact = ContactDatas.GetAll().Except(oldList).FirstOrDefault();
+
+            // Если все контакты добавлены во все группы - создать новую группу и выбрать любой контакт
+            if (contact == null)
+            {
+                app.Groups.Create(new GroupDatacs("TestGroup1", "TestGroup2", "TestGroup3"));
+                group = GroupDatacs.GetAll().Last(); 
+                contact = ContactDatas.GetAll().First(); 
+            }
+
             app.Contacts.AddContactToGroup(contact, group);
+
             List<ContactDatas> newList = group.GetContacts();
             oldList.Add(contact);
             newList.Sort();
@@ -26,17 +48,37 @@ namespace addressbook_web_tets
         [Test]
         public void DeleteContactToGroupTest()
         {
+            // Проверка наличия групп, если нет - создаем
+            if (GroupDatacs.GetAll().Count == 0)
+            {
+                app.Groups.Create(new GroupDatacs("TestGroup1", "TestGroup2", "TestGroup3"));
+            }
+
             GroupDatacs group = GroupDatacs.GetAll()[0];
             List<ContactDatas> oldList = group.GetContacts();
-            ContactDatas contact = oldList.First();
 
-            app.Contacts.DeleteContactToGroup(contact, group);
+            // Если в группе нет контактов - добавляем
+            if (oldList.Count == 0)
+            {
+                // Проверка наличия контактов, если нет - создаем
+                if (ContactDatas.GetAll().Count == 0)
+                {
+                    app.Contacts.Create(new ContactDatas("Test", "Test2"));
+                }
+
+                // Добавляем первый доступный контакт в группу
+                ContactDatas contact = ContactDatas.GetAll().First();
+                app.Contacts.AddContactToGroup(contact, group);
+                oldList.Add(contact); 
+            }
+
+            ContactDatas contactToDelete = oldList.First();
+            app.Contacts.DeleteContactToGroup(contactToDelete, group);
 
             List<ContactDatas> newList = group.GetContacts();
-            oldList.Remove(contact);
+            oldList.Remove(contactToDelete); 
             oldList.Sort();
             newList.Sort();
-
             Assert.AreEqual(oldList, newList);
         }
     }
